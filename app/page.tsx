82dce53393ddab1fd74ec3f13c03eff61d6f9522
Thiago de Oliveira Dantas <t.dod@live.com>
2025-12-01 16:11:08 -0300

    .

--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <div>Pagina Inicial</div>
-  );
-}
--- lib/auth.ts
+++
@@ -1,9 +0,0 @@
-export function parseJwt(token: string) {
-  try {
-    const payload = token.split('.')[1];
-    const json = atob(payload.replace(/-/g, '+').replace(/_/g, '/'));
-    return JSON.parse(decodeURIComponent(escape(json)));
-  } catch {
-    return null;
-  }
-}
